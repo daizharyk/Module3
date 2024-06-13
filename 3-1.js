67e81df4ba9c17612cbf7cd54f1d917b4496e23b
daizharyk <aizharyk76@gmail.com>
2024-06-13 17:23:40 +0500

    Задание 3-3 , Модуль3  (Создал переменные и преобразовал их к различным типам

--- 3-1.js
+++ 3-1.js
@@ -1,9 +1,9 @@
 // Названия статей
-let article1 = "Javascript";
-let article2 = "Technology";
+// let article1 = "Javascript";
+// let article2 = "Technology";
 
-console.log ("Artycle 1 name:");
-console.log (article1);
+// console.log ("Artycle 1 name:");
+// console.log (article1);
 
-console.log ("Artycle 2 name:");
-console.log (article2);
+// console.log ("Artycle 2 name:");
+// console.log (article2);
