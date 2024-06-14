3bf85a62473db0f271b55f8af2b038b20b43c214
daizharyk <aizharyk76@gmail.com>
2024-06-14 15:19:50 +0500

    создал функцию 3-6 задание

---
+++ 3-6.js
@@ -0,0 +1,17 @@
+
+function logarticle(articlename = "new article") {
+  console.log("My new article name is", articlename);
+}
+
+logarticle("JS");
+logarticle();
+
+function exponentiation(x , y){
+  return x ** y;
+}
+
+const arrowexponentiation = (x,y) => {
+  return x ** y;
+}
+
+const arrowexponentiation2 = (x,y) => x ** y;
