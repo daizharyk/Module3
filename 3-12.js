e7671d25c8760ece178b63aafb4ebea3d511a242
daizharyk <aizharyk76@gmail.com>
2024-07-01 09:45:28 +0500

    if and switch Вествления в коде

---
+++ 3-12.js
@@ -0,0 +1,25 @@
+let user = {
+  name: "Alex",
+  age : 27 ,
+  articles : [{ name: "React"},{ name: "JavaScript"},{ name:"New cource"}],
+};
+
+
+const  getUserArticlesCount = (user) = >{
+  let countArticles = user.articles.length;
+  getUserArticlesCount (`User has ${countArticles} articles`);
+  switch (countArticles) {
+    case 0:
+    case 1:
+    console.log('Нужно добавить больше статей!');
+    break;
+    case 2:
+    case 3:
+    console.log('Отличное начало!');
+    break;
+    default:
+    console.log('Так держать!');
+    break;
+}
+}
+getUserArticlesCount(user);
